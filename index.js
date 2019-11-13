3e366b980f1f5798752afd4cf7aa8829e4fd50a2
aniket <aniket_narkhede94@gmail.com>
2019-11-13 14:54:45 +0530

    my first page

---
+++ index.js
@@ -0,0 +1,27 @@
+const express=require("express");
+const mysql=require("mysql");
+const app=express();
+
+app.get("/",function(req,res)
+{
+    console.log("fetcha data");
+    res.send("fetch data");
+});
+
+app.post("/",function(req,res)
+{
+    console.log("insert data");
+    res.send("insert data");
+});
+
+app.delete("/",function(req,res)
+{
+    console.log("delete data");
+    res.send("delete data");
+});
+
+app.put("/",function(req,res)
+{
+    console.log("update data");
+    res.send("updated data");
+});
